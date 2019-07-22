c45e93fdbc68737772d572669d2a8ab7c2d2fd4f
yulia <yuliazo@ac.sce.ac.il>
2019-07-22 22:40:23 +0300

    fix nodes

---
+++ form-builder/build/precache-manifest.775ba4d3e58161121fd5f3e0f7f9af02.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e3a962399eac024397d46184ea2095",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e590038efef1e2814ba3",
+    "url": "/static/js/2.b41ca3d9.chunk.js"
+  },
+  {
+    "revision": "64fd77a58b4cb4dc4c74",
+    "url": "/static/js/main.05f707b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
